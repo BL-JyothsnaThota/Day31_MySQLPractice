179348d793267056039de65bebc3971d01ebfd11
BL-JyothsnaThota <jyothsna.thota@bridgelabz.com>
2026-06-08 10:24:07 +0530

    ablity to create table

--- MysqlPractice.sql
+++ MysqlPractice.sql
@@ -1,1 +1,11 @@
 //
+CREATE DATABASE payroll_service;
+SHOW DATABASES;
+USE payroll_service;
+
+CREATE TABLE employee_payroll (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    salary DOUBLE,
+    start DATE
+);
