8b2f3dd1b8fd37c2e982b55a5e63916f3650a735
BL-JyothsnaThota <jyothsna.thota@bridgelabz.com>
2026-06-08 10:25:55 +0530

    Ability to insert data into table

--- MysqlPractice.sql
+++ MysqlPractice.sql
@@ -9,3 +9,8 @@ CREATE TABLE employee_payroll (
     salary DOUBLE,
     start DATE
 );
+
+INSERT INTO employee_payroll (name, salary, start) VALUES
+('Bill', 100000, '2018-01-03'),
+('Charlie', 120000, '2019-11-13'),
+('Terisa', 90000, '2020-05-21');
